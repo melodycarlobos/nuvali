16847c00c5b2d9312a7b0671393080c1645042ed
Charles Darwin D.C. Pobre <cdpobre@yahoo.com>
2013-03-01 12:15:20 -0800

    js tweaked

--- wp-content/themes/nuvali/static/nuvali/js/whats_nu.js
+++ wp-content/themes/nuvali/static/nuvali/js/whats_nu.js
@@ -1,30 +1,43 @@
 $(document).ready(function(){
-	var $slider_images = $('.pic-object-container .pic-object-wrapper').find('.pic-object');
-	var $slider_images_count = $slider_images.length;
-	var $slider_wrapper = $('.pic-object-container .pic-object-wrapper');
-	var $slider_wrapper_width = 0;
-	var $slider_container_count = 0;
-	var $slider_container_index = 0;
-	var $slider_current_position = 0;
-	var $slider_remainder = 0;
+		var $slider_wrapper_width = 0;
+		var $slider_container_count = 0;
+		var $slider_container_index = 0;
+		var $slider_current_position = 0;
+		var $slider_remainder = 0;
 
-	$slider_images.each(function(index){
+		$('.slider').each(function(index){
+			$(this).addClass('' + (index+1) + '');
+			$(this).find('.buttons.left').addClass('' + (index+1) + '');
+			$(this).find('.buttons.right').addClass('' + (index+1) + '');
+	
+			console.log($(this).attr('class') + 'pota');
+		});
 
-		$slider_wrapper_width += parseInt($slider_images.eq(index).css('width').split('px')[0]);
+	$('.slider').children('.buttons.right').click(function(){
+		var $class_id = $(this).attr('class').split(' ')[2];
+		console.log($class_id + 'IS THE CLASS');
+		var $slider_images = $('.slider.' + $class_id).find('.pic-object');
+		var $slider_images_count = $slider_images.length;
+		var $slider_wrapper = $('.slider.' + $class_id).find('.pic-object-wrapper');
+	
 
-	});
+		$slider_images.each(function(index){
 
-	$slider_wrapper.css('width', $slider_wrapper_width + 'px');
+			$slider_wrapper_width += parseInt($slider_images.eq(index).css('width').split('px')[0]);
 
-	if(($slider_images_count%4) === 0){
-		$slider_container_count = $slider_images_count / 4;
-	}else{
-		$slider_remainder = $slider_images_count % 4;
-		$slider_container_count +=1;
-		$slider_container_count = parseInt($slider_images_count / 4);
-	}
+		});
 
-	$('.slider').find('.buttons.right').click(function(){
+		$slider_wrapper.css('width', $slider_wrapper_width + 'px');
+
+		if(($slider_images_count%4) === 0){
+			$slider_container_count = $slider_images_count / 4;
+		}else{
+			$slider_remainder = $slider_images_count % 4;
+			$slider_container_count +=1;
+			$slider_container_count = parseInt($slider_images_count / 4);
+		}
+
+
 		if($slider_container_index < ($slider_container_count-1) ){
 			$slider_container_index += 1;
 			$slider_current_position += 516;
@@ -33,7 +46,7 @@ $(document).ready(function(){
 				left: ['-' + $slider_current_position + 'px', 'swing']
 			},1000);
 		}else if(($slider_container_index === ($slider_container_count-1))&&($slider_remainder > 0)){
-			console.log("DAAN KA D2!!!");
+			console.log("DAAN KA D2 UTANG NA LOOB!!!");
 			$slider_container_index += 1;
 			$slider_current_position += (129 * $slider_remainder);
 
@@ -52,10 +65,31 @@ $(document).ready(function(){
 		console.log($slider_current_position);
 	});
 
-	$('.slider').find('.buttons.left').click(function(){
-		// $slider_wrapper.animate({
-		// 	left: ['0px', 'swing']
-		// },1000);
+	$('.slider').children('.buttons.left').click(function(){
+		var $class_id = $(this).attr('class').split(' ')[2];
+
+		var $slider_images = $('.slider.' + $class_id).find('.pic-object');
+		var $slider_images_count = $slider_images.length;
+		var $slider_wrapper = $('.slider.' + $class_id).find('.pic-object-wrapper');
+	
+
+		$slider_images.each(function(index){
+
+			$slider_wrapper_width += parseInt($slider_images.eq(index).css('width').split('px')[0]);
+
+		});
+
+		$slider_wrapper.css('width', $slider_wrapper_width + 'px');
+
+		if(($slider_images_count%4) === 0){
+			$slider_container_count = $slider_images_count / 4;
+		}else{
+			$slider_remainder = $slider_images_count % 4;
+			$slider_container_count +=1;
+			$slider_container_count = parseInt($slider_images_count / 4);
+		}
+
+
 		if(($slider_container_index < ($slider_container_count))&&($slider_container_index > 0)){
 			$slider_container_index -= 1;
 			$slider_current_position -= 516;
